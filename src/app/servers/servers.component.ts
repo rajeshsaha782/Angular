4b0b199f9de06b8f048cc484a52ba8c2c6203dac
Rajesh saha <33521669+rajeshsaha782@users.noreply.github.com>
2018-12-20 12:32:16 +0600

    Component create

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: 'app-servers', //use as tag
+  //selector: '[app-servers]', //use as attribute
+  selector: '.app-servers', //use as class
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  disable = false;
+  server = "Server Not Created!!";
+  serverName = '';
+  isServerCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.disable = true;
+    }, 5000);
+  }
 
   ngOnInit() {
   }
+  id = 10;
+  name = "Rajesh";
 
+  getServerId() {
+    return this.id;
+  }
+  getServerName() {
+    return this.name;
+  }
+
+  CreateServer() {
+    this.isServerCreated = true;
+    this.server = 'Server Created!! Name:' + this.serverName;
+  }
+  getButtonColor() {
+    return this.disable === true ? 'red' : 'green';
+  }
+  getButtonClass() {
+    return this.disable === true ? 'btn btn-danger' : 'btn btn-success';
+  }
+  onUpdateInput(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
